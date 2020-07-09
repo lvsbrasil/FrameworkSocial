b6b77bb0224decacc908f11eaef0bdec07bd564f
Leandro Victor <68040046+lvsbrasil@users.noreply.github.com>
2020-07-09 14:10:20 -0300

    Commit-03

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel master ola
+                    Laravel master ola sds
                 </div>
 
                 <div class="links">
